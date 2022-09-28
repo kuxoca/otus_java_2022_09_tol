cb0b390cac8f25362154f656fd5a4761cd8b365a
ppzeff <ppzeff@gmail.com>
2022-09-28 21:50:46 +0300

    hw01-gradle

---
+++ hw01-gradle/src/main/java/ru/otus/HelloOtus.java
@@ -0,0 +1,9 @@
+package ru.otus;
+
+import static com.google.common.math.IntMath.pow;
+
+public class HelloOtus {
+    public static void main(String[] args) {
+        System.out.println("pow(10,10)=" + pow(10, 10));
+    }
+}
