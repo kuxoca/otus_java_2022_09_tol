d23de18886f0c85f88081612e289d621419d6772
ppzeff <ppzeff@gmail.com>
2022-10-19 11:08:25 +0300

    hw02 tests pass

--- hw02-generics/src/main/java/ru/otus/homework/CustomerReverseOrder.java
+++ hw02-generics/src/main/java/ru/otus/homework/CustomerReverseOrder.java
@@ -1,16 +1,20 @@
 package ru.otus.homework;
 
 
+import java.util.LinkedList;
+
 public class CustomerReverseOrder {
 
     //todo: 2. надо реализовать методы этого класса
     //надо подобрать подходящую структуру данных, тогда решение будет в "две строчки"
 
-    public void add(Customer customer) {
+    private final LinkedList<Customer> linkedList = new LinkedList<>();
 
+    public void add(Customer customer) {
+        linkedList.add(customer);
     }
 
     public Customer take() {
-        return null; // это "заглушка, чтобы скомилировать"
+        return linkedList.pollLast();
     }
 }
--- hw02-generics/src/main/java/ru/otus/homework/CustomerService.java
+++ hw02-generics/src/main/java/ru/otus/homework/CustomerService.java
@@ -2,22 +2,39 @@ package ru.otus.homework;
 
 
 import java.util.Map;
+import java.util.TreeMap;
 
 public class CustomerService {
 
     //todo: 3. надо реализовать методы этого класса
     //важно подобрать подходящую Map-у, посмотрите на редко используемые методы, они тут полезны
+    private final TreeMap<Customer, String> treeMap = new TreeMap<>((o1, o2) -> (int) (o1.getScores() - o2.getScores()));
 
     public Map.Entry<Customer, String> getSmallest() {
         //Возможно, чтобы реализовать этот метод, потребуется посмотреть как Map.Entry сделан в jdk
-        return null; // это "заглушка, чтобы скомилировать"
+        Map.Entry<Customer, String> customerEntry = treeMap.firstEntry();
+        Customer temp = new Customer(
+                customerEntry.getKey().getId(),
+                customerEntry.getKey().getName(),
+                customerEntry.getKey().getScores());
+
+        return Map.entry(temp, customerEntry.getValue());
     }
 
     public Map.Entry<Customer, String> getNext(Customer customer) {
-        return null; // это "заглушка, чтобы скомилировать"
+        Map.Entry<Customer, String> customerEntry = treeMap.higherEntry(customer);
+        if (customerEntry == null) {
+            return null;
+        }
+        Customer temp = new Customer(
+                customerEntry.getKey().getId(),
+                customerEntry.getKey().getName(),
+                customerEntry.getKey().getScores());
+
+        return Map.entry(temp, customerEntry.getValue());
     }
 
     public void add(Customer customer, String data) {
-
+        treeMap.put(customer, data);
     }
 }
