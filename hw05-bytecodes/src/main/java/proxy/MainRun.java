71617200339295adeb3432c0dd9dbc41f2bdee5f
kuxoca <kuxoca@gmail.com>
2022-12-03 10:42:58 +0300

    hw05-bytecode добавлено скорости

--- hw05-bytecodes/src/main/java/proxy/MainRun.java
+++ hw05-bytecodes/src/main/java/proxy/MainRun.java
@@ -6,5 +6,12 @@ public class MainRun {
         loggingInterface.calculation("p1");
         loggingInterface.calculation("p1", "p2");
         loggingInterface.calculation("p1", "p2", "p3");
+
+        System.out.println("-----------");
+
+        TestLoggingInterface loggingInterfaceSpeed = IocSpeed.createTestLoggingClass();
+        loggingInterfaceSpeed.calculation("p1");
+        loggingInterfaceSpeed.calculation("p1", "p2");
+        loggingInterfaceSpeed.calculation("p1", "p2", "p3");
     }
 }
