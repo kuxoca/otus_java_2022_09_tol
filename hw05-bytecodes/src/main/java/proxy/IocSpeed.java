a0abeff5a1909521b3fa8672f9f467c31b3058fc
kuxoca <kuxoca@gmail.com>
2022-12-03 10:42:58 +0300

    hw05-bytecode add speed

--- hw05-bytecodes/src/main/java/proxy/IocSpeed.java
+++ hw05-bytecodes/src/main/java/proxy/IocSpeed.java
@@ -4,10 +4,11 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.util.Arrays;
+import java.util.List;
 
-class Ioc {
+class IocSpeed {
 
-    private Ioc() {
+    private IocSpeed() {
     }
 
     static TestLoggingInterface createTestLoggingClass() {
@@ -19,16 +20,23 @@ class Ioc {
 
     static class TestLoggingInvocationHandler implements InvocationHandler {
         private final TestLoggingInterface testLoggingClass;
+        private final List<Method> methodList;
 
         TestLoggingInvocationHandler(TestLoggingImp testLoggingClass) {
             this.testLoggingClass = testLoggingClass;
+            this.methodList = Arrays.stream(testLoggingClass.getClass().getMethods())
+                    .filter(el -> el.isAnnotationPresent(Log.class))
+                    .toList();
         }
 
         @Override
         public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-            if (testLoggingClass.getClass().getMethod(method.getName(), method.getParameterTypes()).isAnnotationPresent(Log.class)) {
-                System.out.println("executed method:" + method.getName() + ", " +
-                        "param: " + Arrays.toString(args));
+            for (Method methodElement : methodList) {
+                if (methodElement.getName().equals(method.getName())
+                        && Arrays.equals(methodElement.getParameterTypes(), method.getParameterTypes())) {
+                    System.out.println("executed method:" + method.getName() + ", " +
+                            "param: " + Arrays.toString(args));
+                }
             }
             return method.invoke(testLoggingClass, args);
         }
