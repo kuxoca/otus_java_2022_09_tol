3e28e93cdbf9602f4d54dd3e2b452c5fb5a2fa0c
kuxoca <kuxoca@gmail.com>
2023-01-07 15:19:30 +0300

    hw11-JPQL done

---
+++ hw11-JPQL/src/main/java/ru/otus/crm/model/Address.java
@@ -0,0 +1,25 @@
+package ru.otus.crm.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "street")
+    private String street;
+
+    public Address(Long id, String street) {
+        this.id = id;
+        this.street = street;
+    }
+}
