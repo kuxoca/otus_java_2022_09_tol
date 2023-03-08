dd3935004b01c59ec777fe8a950e1d04f7dc09e8
kuxoca <kuxoca@gmail.com>
2023-03-08 16:42:02 +0300

    hw14-done

--- hw14-di/src/main/java/ru/otus/appcontainer/AppComponentsContainerImpl.java
+++ hw14-di/src/main/java/ru/otus/appcontainer/AppComponentsContainerImpl.java
@@ -1,25 +1,55 @@
 package ru.otus.appcontainer;
 
+import ru.otus.appcontainer.api.AppComponent;
 import ru.otus.appcontainer.api.AppComponentsContainer;
 import ru.otus.appcontainer.api.AppComponentsContainerConfig;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.*;
 
 public class AppComponentsContainerImpl implements AppComponentsContainer {
 
     private final List<Object> appComponents = new ArrayList<>();
     private final Map<String, Object> appComponentsByName = new HashMap<>();
 
-    public AppComponentsContainerImpl(Class<?> initialConfigClass) {
+    public AppComponentsContainerImpl(Class<?> initialConfigClass) throws InvocationTargetException, IllegalAccessException, NoSuchMethodException, InstantiationException {
         processConfig(initialConfigClass);
     }
 
-    private void processConfig(Class<?> configClass) {
+    private void processConfig(Class<?> configClass) throws InvocationTargetException, IllegalAccessException, NoSuchMethodException, InstantiationException {
         checkConfigClass(configClass);
-        // You code here...
+
+        var instance = configClass.getConstructor().newInstance();
+        var methods = configClass.getMethods();
+
+        var sortedAnnotateMethods = Arrays.stream(methods).filter(method -> method.isAnnotationPresent(AppComponent.class)).sorted(Comparator.comparing(method -> method.getAnnotation(AppComponent.class).order())).toList();
+
+        for (Method method : sortedAnnotateMethods) {
+            var types = method.getParameterTypes();
+
+            Object[] args = new Object[types.length];
+
+            for (int i = 0; i < types.length; i++) {
+                for (Object el : appComponents) {
+                    if (types[i].isAssignableFrom(el.getClass())) {
+                        args[i] = el;
+                        break;
+                    }
+                }
+                if (args[i] == null) {
+                    throw new NoSuchMethodException();
+                }
+            }
+
+            if (appComponentsByName.containsKey(method.getAnnotation(AppComponent.class).name())) {
+                throw new NoSuchMethodException();
+            }
+
+            var obj = method.invoke(instance, args);
+            appComponents.add(obj);
+            appComponentsByName.put(method.getAnnotation(AppComponent.class).name(), obj);
+        }
     }
 
     private void checkConfigClass(Class<?> configClass) {
@@ -29,12 +59,25 @@ public class AppComponentsContainerImpl implements AppComponentsContainer {
     }
 
     @Override
-    public <C> C getAppComponent(Class<C> componentClass) {
-        return null;
+    public <C> C getAppComponent(Class<C> componentClass) throws NoSuchMethodException {
+
+        Object component = null;
+        for (Object appComponent : appComponents) {
+            if (componentClass.isAssignableFrom(appComponent.getClass())) {
+                if (component != null) {
+                    throw new NoSuchMethodException();
+                }
+                component = appComponent;
+            }
+        }
+        if (component != null) {
+            return (C) component;
+        }
+        throw new NoSuchMethodException();
     }
 
     @Override
     public <C> C getAppComponent(String componentName) {
-        return null;
+        return (C) appComponentsByName.get(componentName);
     }
 }
